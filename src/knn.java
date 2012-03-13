52474f3f45710d1a258ad9ece796d3bfb59e6650
Abhijith Padmakumar <email.abhijith@gmail.com>
2012-03-13 17:02:59 +0800

    Optimized file reading and added code to check time cost of sub-components of the code.

--- src/knn.java
+++ src/knn.java
@@ -2,10 +2,8 @@
    args[1] -> Test File Name
    args[2] -> K-Value
    args[3] -> Metric Type
-
    Uncomment all comments beginning with '#' if you want to see the 'processing' when running the program, but note that it increases time cost.
 */
-
 import java.util.*;
 import java.io.*;
 
@@ -21,71 +19,87 @@ public class knn{
 		/*trainingData is an ArrayList of type Instance (Check instance.java). Each entry of trainingData corresponds to one instance of the training file. See the comments in instance.java for more info about ArrayList constructors. We can tweak the initial capcity of 'trainingData' & 'testData' to optimum value depending on our results. */
 		readData(args[0], trainingData);
 		readData(args[1], testData);
+		final Long fTime = System.nanoTime();
+		System.out.println("File Reading Time: "+(fTime-startTime)/(Math.pow(10,9))+" seconds.");
 		normalize(trainingData);
 		normalize(testData);
+		System.out.println("Normalization Time: "+(System.nanoTime()-fTime)/(Math.pow(10,9))+" seconds.");
 		final int kValue = Integer.parseInt(args[2]);
 		final int dMetric = Integer.parseInt(args[3]);
 		final String fName = args[0].substring(0,args[0].lastIndexOf(".train"))+"_prediction_file.txt";
 		try{
+			Long eTime = System.nanoTime();
 			final BufferedWriter writer = new BufferedWriter(new FileWriter(fName));
 			if(dMetric == 0){
                             euclideanDistance(kValue,dMetric,writer);
                         }else{
-                            //cosineSimilarity(kValue,dMetric,writer);
+                            cosineSimilarity(kValue,dMetric,writer);
                         }
 			writer.close();
 			final long estimatedTime = System.nanoTime() - startTime;
+			Long gTime = System.nanoTime();
 			System.out.println("Time Cost: "+estimatedTime/(Math.pow(10,9))+" seconds.");
+			System.out.println("Distnace Method Time: "+(eTime-gTime)/(Math.pow(10,9))+" seconds.");
 		}
 		catch(IOException e){
 			System.out.println(e.getMessage());
 		}
-
 	}
 
 	/* Method to normalize the data sets */
 
 	public static void normalize(List<Instance> tData){
-		for(int i=0;i<tData.size();i++){
-			int currentIndex; double max,min,normalizedValue;
-			for(Map.Entry<Integer,Double> entry: tData.get(i).parameters.entrySet()){
-				currentIndex = entry.getKey();
+		int currentIndex; double max,min, normalizedValue;
+		for(int i=0;i<tData.size();i++){		
+			for(int j=0;j<tData.get(i).indexes.size();j++){
+				currentIndex = tData.get(i).indexes.get(j);
 				max = Instance.max.get(currentIndex);
 				min = Instance.min.get(currentIndex);
 				if(!(max==min)){
-					normalizedValue = (entry.getValue()-min)/(max-min);
-					tData.get(i).parameters.put(entry.getKey(),normalizedValue);
-				}
-			}			
-		}		
+					normalizedValue = (tData.get(i).values.get(j)-min)/(max-min);
+					tData.get(i).values.set(j,normalizedValue);
+				}					
+			}
+		}
+		double[] absValues = new double[tData.size()];
+		for(int i=0;i<tData.size();i++){
+			absValues[i] = 0;
+			for(int j=0;j<tData.get(i).indexes.size();j++){
+				absValues[i] += Math.pow(tData.get(i).values.get(j),2);
+			}
+			absValues[i] = Math.sqrt(absValues[i]);
+		}
 	}
 
-	/*Method to read data from files. */
 	public static void readData(String fileName, List<Instance> tData){
-			try{   
-                final Scanner reader = new Scanner(new File(fileName));
-                Instance instance; Scanner termReader;
-				do{ 		
-					instance = new Instance(reader.next());
-					Scanner lineReader = new Scanner(reader.nextLine());
-					while(lineReader.hasNext()){
-						termReader = new Scanner(lineReader.next());
-						termReader.useDelimiter(":");
-						instance.addTerm(Integer.parseInt(termReader.next()),Double.parseDouble(termReader.next()));
+		try{
+			String line; int flag=0; int colPos; Instance instance = new Instance("");
+			BufferedReader br = new BufferedReader(
+									new FileReader(fileName));
+			while((line=br.readLine())!=null){
+				for(String token: line.split("\\s+")){
+					if(flag==0){
+						instance = new Instance(token);
+						flag++;
+					}else{
+						colPos = token.indexOf(":");
+						instance.addTerm(Integer.parseInt(token.substring(0,colPos)),Double.parseDouble(token.substring(colPos+1)));				
 					}
-					tData.add(instance);
-                }while(reader.hasNext());
+				}
+				tData.add(instance);
+				flag = 0;
 			}
-			catch(IOException e){
-				System.out.println("File IO Error "+e);
-			}
+			br.close();
+		}
+		catch(IOException e){
+			System.out.println("File IO Error "+e.getMessage());
+		}
 	}
 
 	public static void euclideanDistance(int kValue, int dMetric, BufferedWriter writer){
 		/* In this method, we are looping over all the test Data Instances, calculating the distance eucledian distance between each of these instances and all the training data instances. These distance values are then stored in an array of objects of type Distance (check instance.java). The distances are then sorted and passed to method 'Classification' with a reference to the test Data ID. */
-		float tp = 0; Distances tempDist = new Distances(); 
-        Map.Entry<Integer,Double> TSTentry; 
-        Map.Entry<Integer,Double>TRNentry;
+		float tp = 0; Distances tempDist = new Distances();
+        int trdInstanceDimensionSize, tstInstanceDimensionSize; int _tstIndex = 0; int _trnIndex = 0;
 		//Iterating over all testData Instances
 
 		for(int a = 0; a<testData.size();a++){
@@ -97,45 +111,46 @@ public class knn{
                 tempDist.distanceValue = 0;
                 tempDist.fromInstance = 0;
 
-				Iterator TSTIterator = testData.get(a).parameters.entrySet().iterator();
-				Iterator TRNIterator = trainingData.get(b).parameters.entrySet().iterator();
+				trdInstanceDimensionSize = trainingData.get(b).indexes.size(); //Number of Dimensions of the current trainingData instance
+				tstInstanceDimensionSize = testData.get(a).indexes.size(); //Number of Dimensions of the current testData instance
 
 				/*Loop to calculate the distance between current trainingData instance & testData Instance. i & j keep track of which index we are on in the testData & trainingData instance respectively. */
-
-				do{
+				for(int i = 0, j = 0; ; ){
 					//During each iteration of this for loop, we first check if we have finished traversing through through the testData or trainingData instance
-
-					if(!TSTIterator.hasNext()){
-						if(TRNIterator.hasNext()){
+					if(i == tstInstanceDimensionSize){
+						if(!(j == trdInstanceDimensionSize)){
 							do{
-								TRNentry = (Map.Entry)TRNIterator.next();
-								tempDist.distanceValue += Math.pow(TRNentry.getValue(),2);
-							}while(TRNIterator.hasNext());
+								tempDist.distanceValue += Math.pow(trainingData.get(b).values.get(j),2);
+								j++;
+							}while(j != trdInstanceDimensionSize);
 						}
 						break;
-					}	else if(!TRNIterator.hasNext()){
-								do{
-									TSTentry = (Map.Entry)TSTIterator.next();
-									tempDist.distanceValue += Math.pow(TSTentry.getValue(),2);
-								}while(TSTIterator.hasNext());
-								break;
+					}else if(j == trdInstanceDimensionSize){
+							do{
+								tempDist.distanceValue += Math.pow(testData.get(a).values.get(i),2);
+								i++;
+							}while(i != tstInstanceDimensionSize);
+							break;
 					// The next 'if' statement is reached only if the there are indexes remaining in both the test and training data instances.
-					}	else{
-							TSTentry = (Map.Entry)TSTIterator.next();
-							TRNentry = (Map.Entry)TRNIterator.next();
-							if(TSTentry.getKey()==TSTentry.getKey()){
-								tempDist.distanceValue += Math.pow((TSTentry.getValue()-TRNentry.getValue()),2);		
-					/* The next 'if' statement is reached only if the current indexes of the instances are different and if the testData index is lesser thanthe trainingData index, implying that the training data has a missing dimension */				
-							}else if(TSTentry.getKey()<TRNentry.getKey()){
-								tempDist.distanceValue += Math.pow(TSTentry.getValue(),2);
+					}else{
+							_tstIndex = testData.get(a).indexes.get(i);
+							_trnIndex = trainingData.get(b).indexes.get(j);
+							if(_tstIndex==_trnIndex){
+								tempDist.distanceValue += Math.pow((testData.get(a).values.get(i)-trainingData.get(b).values.get(j)),2);
+								i++; j++;
+					/* The next 'if' statement is reached only if the current indexes of the instances are different and if the testData index is lesser than
+					the trainingData index, implying that the training data has a missing dimension */
+							}else if(_tstIndex<_trnIndex){
+								tempDist.distanceValue += Math.pow(testData.get(a).values.get(i),2);
+								i++;
 							}else{
-								//This implies that the testData has a missing dimension.
-								tempDist.distanceValue += Math.pow(TRNentry.getValue(),2);
+						//This implies that the testData has a missing dimension.
+								tempDist.distanceValue += Math.pow(trainingData.get(b).values.get(j),2);
+								j++;
 							}
-					}
-				
-				}while(TSTIterator.hasNext() || TRNIterator.hasNext());
-									
+						}
+				}
+
 				tempDist.distanceValue = Math.sqrt(tempDist.distanceValue);
                 tempDist.fromInstance = b;
 
@@ -155,7 +170,7 @@ public class knn{
 	}
 
 	//Very similar as eucledian distance, except cosine similarity is calculated.
-/*	public static void cosineSimilarity(int kValue, int dMetric, BufferedWriter writer){
+	public static void cosineSimilarity(int kValue, int dMetric, BufferedWriter writer){
 		float tp = 0; 
 		double modTstDataInstance = 0, modTrnDataInstance = 0; //Variables to store the mod values of the instances.
         Similarities tempSim = new Similarities();
@@ -204,21 +219,20 @@ public class knn{
                 	metricData.put(tempSim.similarityValue,tempSim.fromInstance);
                 }
 			}
-
 			tp += classification(a,kValue,dMetric,writer);
 		}
 
 		//System.out.println("**************************************");
 		//System.out.println("TP: "+tp+" FP: "+(testData.size()-tp));
 		System.out.println("Accuracy: "+(tp/testData.size())*100);
-	}*/
+	}
 
 	public static int classification(int testInstanceId, int kValue, int dMetric, BufferedWriter writer){
 		try{
 			String classId;
 			double weight = 0;
 			//HashMap to store Key,Value pairs where Key - Class Name and Value is weighted distance.
-			Map<String,Double> classMap = new HashMap<String,Double>(kValue);
+			HashMap<String,Double> classMap = new HashMap<String,Double>(kValue);
 			//Loop to populate the 'classMap' with data from the k closest instances.
 		
 			for(Map.Entry<Double,Integer> entry: metricData.entrySet()){
@@ -248,7 +262,7 @@ public class knn{
 			classId = "0"; //temporary variable to store the class Key of the best Class (class with highest weight)
 			weight = 0;
 			//bestClass is a temporary variable to store the bestClass name and weighted Distance.
-			Map.Entry<String,Double> bestClass = new AbstractMap.SimpleEntry<String,Double>("0",0.0); 
+			AbstractMap.SimpleEntry<String,Double> bestClass = new AbstractMap.SimpleEntry<String,Double>("0",0.0); 
 			for (Map.Entry<String, Double> entry : classMap.entrySet()) {
 	 	   		String key = entry.getKey();
 	   			double value = entry.getValue();
