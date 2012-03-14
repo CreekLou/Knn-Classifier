9cf6d4041ab50b304c2c7fc4833cee4d0a9b68a0
Abhijith Padmakumar <email.abhijith@gmail.com>
2012-03-15 05:18:36 +0800

    Novel feature 0.3 - Code running correctly when classifying training data using training data itself

--- src/knn.java
+++ src/knn.java
@@ -23,11 +23,11 @@ public class knn{
 		final Long fTime = System.nanoTime();
 		//System.out.println("File Reading Time: "+(fTime-startTime)/(Math.pow(10,9))+" seconds.");
 
-/*		normalizeNEW(trainingData,Instance.meanTRN,Instance.stdDevTRN);
-		normalizeNEW(testData,Instance.meanTST,Instance.stdDevTST);*/
+		normalizeNEW(trainingData,Instance.meanTRN,Instance.stdDevTRN);
+		normalizeNEW(testData,Instance.meanTST,Instance.stdDevTST);
 
-		normalize(trainingData,Instance.maxTRN,Instance.maxTST);
-		normalize(testData,Instance.maxTST,Instance.maxTST);
+		//normalize(trainingData,Instance.maxTRN,Instance.maxTST);
+	//	normalize(testData,Instance.maxTST,Instance.maxTST);
 
 		//System.out.println("Normalization Time: "+(System.nanoTime()-fTime)/(Math.pow(10,9))+" seconds.");
 		final int kValue = Integer.parseInt(args[2]);
@@ -37,9 +37,9 @@ public class knn{
 			Long eTime = System.nanoTime();
 			final BufferedWriter writer = new BufferedWriter(new FileWriter(fName));
 			if(dMetric == 0){
-                euclideanDistance(kValue,dMetric,writer,trainingData,testData);
+                euclideanDistance(kValue,dMetric,writer,trainingData,trainingData);
             }else{
-                cosineSimilarity(kValue,dMetric,writer);
+           //     cosineSimilarity(kValue,dMetric,writer);
             }
 			writer.close();
 			final long estimatedTime = System.nanoTime() - startTime;
@@ -136,25 +136,23 @@ public class knn{
 		}
 	}
 
-	public static void euclideanDistance(int kValue, int dMetric, BufferedWriter writer, List<Instance> trainingData, List<Instance> testData){
+	public static void euclideanDistance(int kValue, int dMetric, BufferedWriter writer, List<Instance> trnData, List<Instance> tstData){
 		/* In this method, we are looping over all the test Data Instances, calculating the distance eucledian distance between each of these instances and all the training data instances. These distance values are then stored in an array of objects of type Distance (check instance.java). The distances are then sorted and passed to method 'Classification' with a reference to the test Data ID. */
 		float tp = 0; Distances tempDist = new Distances(); double zz; double estTime = 0.0;
         int trdInstanceDimensionSize, tstInstanceDimensionSize; int _tstIndex = 0; int _trnIndex = 0;
 		//Iterating over all testData Instances
 
-		for(int a = 0; a<testData.size();a++){
+		for(int a = 0; a<tstData.size();a++){
 			//Iterating over all TrainingData Instances
 			metricData.clear();
-
 			zz = System.nanoTime();
 
 			for(int b=0; b<trainingData.size();b++){
-
                 tempDist.distanceValue = 0;
                 tempDist.fromInstance = 0;
 
-				trdInstanceDimensionSize = trainingData.get(b).indexes.size(); //Number of Dimensions of the current trainingData instance
-				tstInstanceDimensionSize = testData.get(a).indexes.size(); //Number of Dimensions of the current testData instance
+				trdInstanceDimensionSize = trnData.get(b).indexes.size(); //Number of Dimensions of the current trainingData instance
+				tstInstanceDimensionSize = tstData.get(a).indexes.size(); //Number of Dimensions of the current testData instance
 
 				/*Loop to calculate the distance between current trainingData instance & testData Instance. i & j keep track of which index we are on in the testData & trainingData instance respectively. */
 				for(int i = 0, j = 0; ; ){
@@ -162,32 +160,32 @@ public class knn{
 					if(i == tstInstanceDimensionSize){
 						if(!(j == trdInstanceDimensionSize)){
 							do{
-								tempDist.distanceValue += Math.pow(trainingData.get(b).values.get(j),2);
+								tempDist.distanceValue += Math.pow(trnData.get(b).values.get(j),2);
 								j++;
 							}while(j != trdInstanceDimensionSize);
 						}
 						break;
 					}else if(j == trdInstanceDimensionSize){
 							do{
-								tempDist.distanceValue += Math.pow(testData.get(a).values.get(i),2);
+								tempDist.distanceValue += Math.pow(tstData.get(a).values.get(i),2);
 								i++;
 							}while(i != tstInstanceDimensionSize);
 							break;
 					// The next 'if' statement is reached only if the there are indexes remaining in both the test and training data instances.
 					}else{
-							_tstIndex = testData.get(a).indexes.get(i);
-							_trnIndex = trainingData.get(b).indexes.get(j);
+							_tstIndex = tstData.get(a).indexes.get(i);
+							_trnIndex = trnData.get(b).indexes.get(j);
 							if(_tstIndex==_trnIndex){
-								tempDist.distanceValue += Math.pow((testData.get(a).values.get(i)-trainingData.get(b).values.get(j)),2);
+								tempDist.distanceValue += Math.pow((tstData.get(a).values.get(i)-trnData.get(b).values.get(j)),2);
 								i++; j++;
 					/* The next 'if' statement is reached only if the current indexes of the instances are different and if the testData index is lesser than
 					the trainingData index, implying that the training data has a missing dimension */
 							}else if(_tstIndex<_trnIndex){
-								tempDist.distanceValue += Math.pow(testData.get(a).values.get(i),2);
+								tempDist.distanceValue += Math.pow(tstData.get(a).values.get(i),2);
 								i++;
 							}else{
 						//This implies that the testData has a missing dimension.
-								tempDist.distanceValue += Math.pow(trainingData.get(b).values.get(j),2);
+								tempDist.distanceValue += Math.pow(trnData.get(b).values.get(j),2);
 								j++;
 							}
 						}
@@ -206,20 +204,20 @@ public class knn{
                 	}
 				}
 			}
-
-			System.out.println(metricData.entrySet());
-
-
+		/*//	System.out.println("TEST ID: "+a);
+			for(Map.Entry<Double,Integer> entry: metricData.entrySet()){
+				System.out.println("Dist: "+entry.getKey()+" from: "+entry.getValue()+" CLass: "+trnData.get(entry.getValue()).instanceClass);
+			}*/
 
 			//The classification method returns 1 if the current instance is classified accurately, else returns 0. 'tp' keeps a track of the true positives.
-			tp += classification(a,kValue,dMetric,writer);
+			tp += classification(a,kValue,dMetric,writer,trnData,tstData);
 			estTime += (System.nanoTime()-zz)/(Math.pow(10,9));
 		}
-		System.out.println("Accuracy: "+(tp/testData.size())*100);
+		System.out.println("Accuracy: "+(tp/tstData.size())*100);
 	}
 
 	//Very similar as eucledian distance, except cosine similarity is calculated.
-	public static void cosineSimilarity(int kValue, int dMetric, BufferedWriter writer){
+/*	public static void cosineSimilarity(int kValue, int dMetric, BufferedWriter writer){
 		float tp = 0; 
 		double modTstDataInstance = 0, modTrnDataInstance = 0; //Variables to store the mod values of the instances.
         Similarities tempSim = new Similarities();
@@ -275,8 +273,8 @@ public class knn{
 		//System.out.println("TP: "+tp+" FP: "+(testData.size()-tp));
 		System.out.println("Accuracy: "+(tp/testData.size())*100);
 	}
-
-	public static int classification(int testInstanceId, int kValue, int dMetric, BufferedWriter writer){
+*/
+	public static int classification(int testInstanceId, int kValue, int dMetric, BufferedWriter writer, List<Instance> trnData, List<Instance> tstData){
 		try{
 			String classId;
 			double weight = 0;
@@ -294,7 +292,7 @@ public class knn{
 				}else{
 					//#System.out.print("Similarity: "+entry.getKey()+" between Test Instance: "+testInstanceId+" and Training Instance: "+entry.getValue());
 					//#System.out.println(" Class: "+trainingData.get(entry.getValue()).instanceClass);
-					classId = trainingData.get(entry.getValue()).instanceClass;
+					classId = trnData.get(entry.getValue()).instanceClass;
 					weight = entry.getKey(); 
 					//#System.out.println("Weight: "+weight);
 				}
@@ -305,7 +303,7 @@ public class knn{
 					classMap.put(classId,weight);
 				}
 			}
-			System.out.println(classMap.entrySet());
+			//System.out.println(classMap.entrySet());
 
 			classId = "0"; //temporary variable to store the class Key of the best Class (class with highest weight)
 			weight = 0;
@@ -320,16 +318,18 @@ public class knn{
 	   			}
 	    	}
 
+/*	    	System.out.println("Assigned CLass: "+bestClass.getKey());
+	    	System.out.println("Actual Class: "+tstData.get(testInstanceId).instanceClass);*/
+
 	    	writer.write(bestClass.getKey()+"");
 	    	writer.newLine();
 			
-		    System.out.println();
 	    	//Checking if the bestClass the model determined is equal to the actual class.
-	    	testData.get(testInstanceId).classifiedClass = (String)bestClass.getKey();
+	    	tstData.get(testInstanceId).classifiedClass = (String)bestClass.getKey();
 /*	    	System.out.println("Best Class : "+testData.get(testInstanceId).classifiedClass);
 	    	System.out.println("Actual Class: "+testData.get(testInstanceId).instanceClass);
 			System.out.println("-------------------------------------------------------------------");*/
-	    	if(testData.get(testInstanceId).classifiedClass.equals(testData.get(testInstanceId).instanceClass)){
+	    	if(tstData.get(testInstanceId).classifiedClass.equals(tstData.get(testInstanceId).instanceClass)){
 	    		return 1;
 	    	}else{
 	    		return 0;
